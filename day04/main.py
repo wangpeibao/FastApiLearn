cffe7fbc1f19d28219d9d317ace8cdab04e747a8
wangpeibao <wangpeibao@troila.com>
2020-05-27 13:35:48 +0800

    day04

--- day04/main.py
+++ day04/main.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Union, Dict
 
 import uvicorn
-from fastapi import FastAPI, Cookie, Header
+from fastapi import FastAPI, Cookie, Header, Form, File, UploadFile
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -54,6 +54,64 @@ def request09(*, item: Item):
 @app.post("/request10", response_model=Item, response_model_exclude={"age"})
 def request10(*, item: Item):
     return item
+
+class Item1(BaseModel):
+    name: str
+    age: int = 1
+
+class Item2(BaseModel):
+    name: str
+    age: int
+    alias_name: str = "别名"
+
+class Item3(Item1):
+    alias_name: str = "别名"
+
+class Item4(BaseModel):
+    alias_name: str = "别名"
+
+@app.post("/request11")
+def request11(*, item: Item1):
+    print(item.dict())
+    item2 = Item2(**item.dict())
+    return item2
+
+@app.post("/request12")
+def request12(*, item: Item1):
+    item3 = Item3(**item.dict(), alias_name="哈哈")
+    return item3
+
+@app.post("/request13", response_model=Union[Item1, Item4])
+def request13(*, item: Item1):
+    return item
+
+@app.post("/request14", response_model=Dict[str, int])
+def reqeust14():
+    return {"a": 1, "b": 2}
+
+@app.post("/request15")
+def request15(name: str = Form(...)):
+    return name
+
+@app.post("/request16")
+def request16(file: bytes = File(...)):
+    return len(file)
+
+@app.post("/request17")
+async def request17(file: UploadFile = File(...)):
+    with open("./%s" % file.filename, "wb") as f:
+        content = await file.read()
+        f.write(content)
+        f.close()
+    return file.filename
+
+@app.post("/reqeust18")
+def request18(files: List[UploadFile] = File(...)):
+    res = []
+    for file in files:
+        res.append(file.filename)
+    return res
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
